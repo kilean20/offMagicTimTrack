9fc37f4723bd62edea44089855f46dd19d1b5c78
kilean20 <kilean20@gmail.com>
2016-05-13 10:13:14 -0400

    [Lin] ran2 for dEm1E-2

--- global.h
+++ global.h
@@ -33,7 +33,7 @@ const double magicBETA2=magicBETA*magicBETA;
 const double eMASS = 0.51099891013; // MeV/c^2
 const double magicENERGY = magicGAMMA*eMASS; //MeV
 
-const double offMagicDelta = 1.0E-3;
+const double offMagicDelta = 1.0E-2;
 //const double offMagicDelta = 0.0;
 
 
