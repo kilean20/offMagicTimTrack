aec0ebe5e701a1950ab21da0421e17ace1884453
kilean20 <kilean20@gmail.com>
2016-07-03 17:14:07 -0700

    [Win] added myroutine.h

---
+++ myroutine.h
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <vector>
+#include "simtrack/elepass.h"
+
+#include <cstdlib> //for gaussian
+
+//void Cal_Sext_Driving(Line & RING, double l);
+
+double unifRand();
+double unifRand(double, double);
+double exponentialrand();
+double trunc_exponentialrand(double);
+double gaussrand();
+double trunc_gaussrand(double);
+double xgaussRand();
+double trunc_xgaussRand(double);
+int factorial(int n);
